210191a36a6ba4a586a2ca5ebddd00f2b9cd3450
andrewhlu <alu@ucsb.edu>
2021-02-07 02:01:47 -0800

    Intro screen and birthday lyrics

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,116 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Head from 'next/head';
+import { useRef, useState } from 'react';
+import styles from '../styles/Index.module.css';
 
-export default function Home() {
-  return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+export default function Index() {
+    const mainDiv = useRef(null);
+    const audioElement = useRef(null)
+    const videoElement = useRef(null);
+    let [welcomeScreen, setWelcomeScreen] = useState(true);
+    let [pastLyrics, setPastLyrics] = useState("");
+    let [futureLyrics, setFutureLyrics] = useState("");
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+    const settings = {
+        name: "Andrew"
+    }
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
+    const liveLyrics = [
+        [5401, "Hap", "py Birthday to You!"],
+        [5556, "Happy ", "Birthday to You!"],
+        [5729, "Happy Birth", "day to You!"],
+        [6048, "Happy Birthday", " to You!"],
+        [7669, "Happy Birthday to", " You!"],
+        [8341, "Happy Birthday to You!", ""],
+        [10605, "Hap", "py Birthday to You!"],
+        [10760, "Happy ", "Birthday to You!"],
+        [10933, "Happy Birth", "day to You!"],
+        [11252, "Happy Birthday", " to You!"],
+        [12873, "Happy Birthday to", " You!"],
+        [13545, "Happy Birthday to You!", ""],
+        [15535, "Hap", `py Birthday to ${settings.name}!`],
+        [15842, "Happy ", `Birthday to ${settings.name}!`],
+        [16195, "Happy Birth", `day to ${settings.name}!`],
+        [17505, "Happy Birthday", ` to ${settings.name}!`],
+        [18147, "Happy Birthday to", ` ${settings.name}!`],
+        [18807, `Happy Birthday to ${settings.name}!`, ""],
+        [20768, "Hap", "py Birthday to You!"],
+        [21066, "Happy ", "Birthday to You!"],
+        [21411, "Happy Birth", "day to You!"],
+        [22318, "Happy Birthday", " to You!"],
+        [23351, "Happy Birthday to", " You!"],
+        [24023, "Happy Birthday to You!", ""],
+    ];
 
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
+    const finishedLoadingVideo = (e) => {
+        console.log("Finished loading video!");
+    }
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
+    const startVideo = () => {
+        setWelcomeScreen(false);
+        mainDiv.current.requestFullscreen();
+        // audioElement.current.currentTime = 40;
+        audioElement.current.play();
 
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
+        setTimeout(() => {
+            setInterval(() => {
+                audioElement.current.currentTime = 5.058;
+            }, 41790);
+        }, 5058);
 
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+        liveLyrics.forEach(lyric => {
+            setTimeout(() => {
+                setPastLyrics(lyric[1]);
+                setFutureLyrics(lyric[2]);
+            }, lyric[0]);
+        })
+
+        // videoElement.current.play();
+        console.log("Playing Video!");
+    }
+
+    const onAudioEnd = () => {
+        // audioElement.current.currentTime = 5.058;
+        // audioElement.current.play();
+        console.log("Looping");
+    }
+
+    const getVideoOpacity = () => {
+        // return welcomeScreen ? 0 : 1;
+        return 0;
+    }
+
+    return (
+        <div ref={mainDiv} className={styles.mainDiv}>
+            <Head>
+                <title>Happy Birthday!</title>
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+
+            <audio ref={audioElement} onCanPlayThrough={finishedLoadingVideo} onEnded={onAudioEnd} src="birthday.m4a"></audio>
+            <video ref={videoElement} className={styles.video} onCanPlayThrough={finishedLoadingVideo} src="https://andrewhlu.com/kailin-bday/video.mp4" style={{opacity: getVideoOpacity()}}></video>
+            
+            {welcomeScreen ?
+                <div className={styles.main}>
+                    <div className={styles.header}>
+                        <h1>Happy Birthday {settings.name}!</h1>
+                        <p>Today's your special day! Since your friends aren't able to meet you in person, they have worked together to present this animation to you as a surprise!</p>
+                    </div>
+
+                    <hr style={{height: "3px", width: "100%", color: "black", backgroundColor: "black"}}></hr>
+
+                    <div className={styles.startDiv}>
+                        <p>This animation is designed to be played on a computer with audio on.</p>
+                        <p>The browser will enter fullscreen mode.</p>
+                        <button className={styles.startButton} onClick={startVideo}>Start Animation</button>
+                    </div>
+                </div>
+            : 
+                <div className={styles.main}>
+                    <div className={styles.header}>
+                        <h1>{pastLyrics}<span className={styles.dim}>{futureLyrics}</span></h1>
+                    </div>
+                </div>
+            }
         </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
-    </div>
-  )
-}
+    )
+};
